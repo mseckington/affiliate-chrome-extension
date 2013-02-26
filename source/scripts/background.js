e07a7da7523da2c50289996fb2d2f5e30e3a6c8b
Melinda Seckington <mseckington@gmail.com>
2013-02-26 12:41:25 +0000

    version 1.1

--- source/scripts/background.js
+++ source/scripts/background.js
@@ -1,5 +1,6 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.message == "showPageAction") {
+		console.log("received message: ShowPageAction");
 		showPageAction(sender.tab)		
   } else if (request.message == "getRewrittenUrl") {
 		sendResponse(rewriteUrl(request.url));
@@ -8,19 +9,34 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 
 var rewriteUrl = function(url) {
 	uri = new Uri(url);
-	tag = tagForUri(uri);
-	uri.addQueryParam('tag', tag);
+	host = uri.host();
+	tag = tagForHost(host);
+	if (host == 'www.threadless.com') {
+		uri.addQueryParam('from', tag);
+	} else if (host == 'www.bookdepository.co.uk') {
+		uri.addQueryParam('a_aid', tag);
+	} else if (host == 'www.bookdepository.com') {
+		uri.addQueryParam('a_aid', tag);
+	} else {
+		uri.addQueryParam('tag', tag);
+	}	
 	return uri.toString();
 }
 
-var tagForUri = function(url) {
-	return window.localStorage.getItem(url.host());
+var tagForHost = function(host) {
+	return window.localStorage.getItem(host);
 }
 
 var showPageAction = function(tab) {
 	uri = new Uri(tab.url);
-	tag = tagForUri(uri);
+	console.log(uri);
+	console.log(uri.host());
+	tag = tagForHost(uri.host());
+	console.log(tag);
 	if (tag != "" && tag != null && tag != undefined) {
+		console.log("showing pageAction");
 		chrome.pageAction.show(tab.id);
+		console.log(tab.id)
+		console.log("^ tab.id")
 	}
 }
